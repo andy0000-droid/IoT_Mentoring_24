f0250a56e70f139a0e3f5fbfcf7f3a42f39171c0
andy0000-droid <andy020207@gmail.com>
2024-09-28 23:58:57 +0900

    Update Initial File

---
+++ AES/key_schedule.c
@@ -0,0 +1,47 @@
+/**
+ * @file key_schedule.c
+ * @author Choi SoonWook (andy2070@korea.ac.kr)
+ * @brief 
+ * @version 0.1
+ * @date 2024-09-28
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <stdio.h>
+
+void ByteSub();
+
+void ShiftRow();
+
+void MixCol();
+
+void AddRK();
+
+int main(void)
+{
+    while(1)
+    {
+
+    }
+}
+
+void ByteSub()
+{
+
+}
+
+void ShiftRow()
+{
+
+}
+
+void MixCol()
+{
+
+}
+
+void AddRK()
+{
+
+}
