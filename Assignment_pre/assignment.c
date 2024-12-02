b82df03c07efc2b3811da395a3ab871cc1562dbb
andy0000-droid <andy020207@gmail.com>
2024-12-03 08:31:06 +0900

    xtimes implement

--- Assignment_pre/assignment.c
+++ Assignment_pre/assignment.c
@@ -1,90 +1,75 @@
-/*
-Left to Right Exponentiation Algorithm
-GF(2^128), Defining Polynomial = x^128 + x^7 + x^2 + x + 1
-0b 10000000 00000000 00000000 00000000 00000000 00000000 00000000 10000111
-0x 80 00 00 00 00 00 00 87
+/**
+ * @brief 
+ * Left to Right Exponentiation Algorithm
+ * GF(2^128), Defining Polynomial = x^128 + x^7 + x^2 + x + 1
+ * 0b 10000000 00000000 00000000 00000000 00000000 00000000 00000000 10000111
+ * 0x 80 00 00 00 00 00 00 87
+ * 
+ * 128 bits Multiplication & Squaring
+ * 128 = 16 * 8 (16 bytes)
+ * 
+ * Exponent: (2bytes)
+ *     0b 00001010 11101101
+ *     0x 0a ed
+ * 
+ * Base: 
+ *     Arbitarily (Hard coded)
+ * 
+ */
 
-128 bits Multiplication & Squaring
-128 = 16 * 8 (16 bytes)
 
-Exponent: (2bytes)
-    0b 00001010 11101101
-    0x 0a ed
 
-Base: 
-    Arbitarily (Hard coded)
-*/
-
-/*
-For GF(2^8)
-#define xtime(A) ((A << 1) ^ ((A & 0x80) ? 0x1b : 0)) //0x02
-#define x2time(A) (xtime(xtime(A))) //0x04
-#define x3time(A) (xtime(x2time(A))) //0x08
-#define x4time(A) (xtime(x3time(A))) //0x10
-#define x5time(A) (xtime(x4time(A))) //0x20
-#define x6time(A) (xtime(x5time(A))) //0x40
-#define x7time(A) (xtime(x6time(A))) //0x80
-
-BYTE mul(BYTE a, BYTE b) {
-    BYTE res = 0;
-    if (b & 0x01) {
-        res ^= a;
-    }
-    if (b & 0x02) {
-        res ^= xtime(a);
-    }
-    if (b & 0x04) {
-        res ^= x2time(a);
-    }
-    if (b & 0x08) {
-        res ^= x3time(a);
-    }
-    if (b & 0x10) {
-        res ^= x4time(a);
-    }
-    if (b & 0x20) {
-        res ^= x5time(a);
-    }
-    if (b & 0x40) {
-        res ^= x6time(a);
-    }
-    if (b & 0x80) {
-        res ^= x7time(a);
-    }
-    return res;
-}
-
-BYTE squaring(BYTE a) {
-    BYTE h = (a & 0xf0) >> 4;
-    BYTE l = a & 0x0f;
-    BYTE ah = 0, al = 0;
-    BYTE res = 0;
-    for (int i = 0; i < 4; i++) {
-        ah ^= (h & (1<<i)) << i;
-        al ^= (l & (1<<i)) << i;
-    }
-    BYTE tah = ah ^ xtime(ah) ^ x3time(ah) ^ x4time(ah);
-    res = tah ^ al;
-    printf("Squaring Result : %02x\n", res);
-    return res;
-}
-*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef unsigned char BYTE;
 
-#define byte 8;
+#define byte 8
 
-BYTE x2(BYTE *a) {
+void x2(BYTE *in, BYTE *res, int size) {
+    /*
+     * Perform xtimes for multiplication
+     */
+    bool cf = false;
+    int i = 0;
+
+    cf = in[0] & 0x80;
+    //printf("Carry Flag: %s\n", cf ? "T" : "F");
+
+    //printf("%d\n", i);
+    res[0] = in[0] << 0x01;
+    i++;
+
+    for (; i < size; i++) {
+        //printf("Index: %02d ", i);
+        BYTE temp = 0;
+        temp = (in[i] & 0x80) >> 0x07; //Most Significant Bit
+        //printf("Carry: %s\n", temp ? "T" : "F");
+        res[i] = in[i] << 0x01;
+        res[i - 1] ^= temp;
+    }
+    if (cf) {
+        res[size] ^= 0x87;
+    }
+}
+
+void mul(BYTE *a, BYTE *b, BYTE *res) {
     
+}
+
+void sq(BYTE *in, BYTE *res) {
+
 }
 
 
 int main() {
-    BYTE base[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}; //16 bytes
-    BYTE exp[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0xed}; //16 bytes
-
-
-
+    BYTE base[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}; //16 bytes with big endian
+    BYTE exp[2] = {0x0a, 0xed}; //2 bytes
+    BYTE res[16] = {0x00, };
+    int size = sizeof(base) / sizeof(char);
+    //x2(base, res, size);
+    /*for (int i = 0; i < size; i++) {
+        printf("0x%02x ", res[i]);
+    }*/
 }
